5654733c3b15600bdb1cd9ead76d966ded59b3e3
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2024-03-21 00:50:56 +0600

    Admin Portion Done

--- src/admin/admin.entity.ts
+++ src/admin/admin.entity.ts
@@ -1,43 +1,238 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { StaffInfo } from "./admin.dto";
+import * as bcrypt from 'bcryptjs';
 
 @Entity("admin")
-export class AdminEntity{
+export class Admin {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    dob: string;
+  
+    @Column()
+    phoneNumber: string;
+
     
+
+    @Column()
+    email: string;
+
+    @Column()
+    designation: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    nid: number;
+
+    @Column()
+    profilepic: string;
+    
+    // @BeforeInsert()
+    // async hashPassword() {
+    //     this.password = await bcrypt.hash(this.password, 10);
+    // }
+
+    @OneToOne(() => Staff)
+    @JoinColumn()
+    staffInfo: Staff[];
+}
+
+@Entity("student")
+export class Student {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    studentId: string;
+  
+    @Column()
+    fullName: string;
+  
+    @Column()
+    email: string;
+
+    @Column()
+    fathersname:string;
+
+    @Column()
+    mothersname:string;
+
+    @Column()
+    guardianname:string;
+
+    @Column()
+    guardianphone:string;
+  
+    @Column()
+    address: string;
+  
+    @Column()
+    phoneNumber: string;
+  
+    
+  
     @Column({ nullable: true })
-    ad_name: string;
-    
-    // @Column({ nullable: true })
-    // ad_username: string;
-    
-    // @Column()
-    // ad_email: string;
-    
-    // @Column()
-    // ad_password: string;
-    
-    @Column({ default: true })
-    isActive: boolean;
+    major: string;
+  
+    @Column({ type: 'float', nullable: true })
+    gpa: number;
+  
+    @Column()
+    dob: string;
+  
+    @Column()
+    password: string;
+  
+    @Column()
+    enrollmentDate: Date;
+  
+    @Column({ type: 'date', nullable: true })
+    graduationDate: Date;
+  
+    @Column()
+    profilepic: string;
 
-    @Column({ type: 'bigint', unsigned: true })
-    ad_phoneNumber: string;
-    
-    // @Column()
-    // ad_age: number;
+    @ManyToMany(() => Course, course => course.students)
+    courses: Course[];
+}
 
-    // @Column()
-    // ad_address: string;
+@Entity("faculty")
+export class Faculty {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ nullable: true })
+    name: string;
+  
+    @Column()
+    username: string;
+  
+    @Column()
+    address: string;
+  
+    @Column()
+    phoneNumber: string;
+  
     
-    // @Column()
-    // ad_nid: number;
+  
+    @Column()
+    email: string;
 
-    @BeforeInsert()
-    generateId() {
-        this.id = Math.floor(Math.random() * 1000000);
-    }
+    @Column()
+    dob: string;
+  
+  
+    @Column()
+    designation: string;
+  
+    @Column()
+    password: string;
+  
+    @Column()
+    nid: number;
+  
+    @Column()
+    profilepic: string;
 
-    
-    // @Column()
-    // filename:string;
+    @OneToMany(() => Course, course => course.faculty)
+    courses: Course[];
+    @ManyToOne(() => Department, department => department.faculty)
+    department: Department[];
+}
+
+@Entity("staff")
+export class Staff {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: true })
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    phoneNumber: string;    
+
+    @Column()
+    dob: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    designation: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    nid: number;
+
+    @Column()
+    profilepic: string;
+
+    @ManyToOne(() => Department, department => department.staff)
+    department: Department[];
+}
+
+@Entity("course")
+export class Course {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    code: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    credits: number;
+
+    @ManyToOne(() => Faculty, faculty => faculty.courses)
+    faculty: Faculty;
+
+    @ManyToMany(() => Student)
+    @JoinTable()
+    students: Student[];
+
+    @ManyToOne(() => Department, department => department.courses)
+    department: Department[];
+}
+
+@Entity("department")
+export class Department {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    degree: string; 
+
+    @OneToMany(() => Staff, staff => staff.department)
+    staff: Staff[];
+
+    @OneToMany(() => Faculty, faculty => faculty.department)
+    faculty: Faculty[];
+
+    @OneToMany(() => Course, course => course.department)
+    courses: Course[];
 }
