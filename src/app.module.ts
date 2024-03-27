2c22353aa7841e27c2530d5294760e8d96339311
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2024-03-28 04:27:23 +0600

    Module Updated after Merging IT Staff Portion

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,15 @@ import { ITStuffModule } from './It Stuff/it_stuff.module';
 
 @Module({
   imports: [AdminModule, TeacherModule, StudentsModule, ITStuffModule, TypeOrmModule.forRoot(
-    { type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '2222',
-    database: 'UMS',
-    autoLoadEntities: true,
-    synchronize: true,
+    { 
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '2222',
+      database: 'UMS',
+      autoLoadEntities: true,
+      synchronize: true,
     } ),
     ],
   controllers: [AppController],
