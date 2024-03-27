540d9d591ab0a6256cf6daf9054606af12d1f1d4
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2024-03-28 04:26:02 +0600

    Admin Portion Done

--- src/admin/admin.session.guard.ts
+++ src/admin/admin.session.guard.ts
@@ -4,9 +4,9 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class SessionGuard implements CanActivate {
     canActivate(
-      context: ExecutionContext,
+        context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
-      const request = context.switchToHttp().getRequest();
-      return request.session.email !== undefined;
+        const request = context.switchToHttp().getRequest();
+        return request.session.email !== undefined;
     }
 }
